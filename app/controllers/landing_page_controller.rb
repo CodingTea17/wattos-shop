19ef0bffabe7826f6bc53c0e043aa54138892118
Dawson M <dawson.mortenson@gmail.com>
2017-11-17 13:02:50 -0800

    add a scope to grab the 3 most recently added products

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,5 +1,6 @@
 class LandingPageController < ApplicationController
   def home
     # Renders (by default) app/views/landing_page/home.html.erb
+    @products = Product.recent
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
   validates :name, :cost, :origin , :presence => true
   has_many :reviews
+
   scope :index, -> (page) { order("name ASC").paginate(:page => page, :per_page => 10) }
+
+  scope :recent, -> { order("created_at DESC").limit(3) }
 end
