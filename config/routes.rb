85e63810b0789e3788d2201bb0f5fa6d7d65c9f9
Dawson M <dawson.mortenson@gmail.com>
2017-11-20 09:10:57 -0800

    trim extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'landing_page#home'
   resources :products do
-    resources :reviews
+    resources :reviews, :except => [:show, :index, :destroy, :edit, :update]
   end
 end
